7006292bdd1ec9e2972764de902a268b0cb9a8d6
Tyler A. Young <tyler@tylerayoung.com>
2025-02-22 07:28:21 -0600

    Add checksum for v0.1.3 release (#5)

--- checksum-Elixir.LRS.exs
+++ checksum-Elixir.LRS.exs
@@ -1,6 +1,8 @@
 %{
-  "liblrs-v0.1.2-nif-2.15-aarch64-apple-darwin.so.tar.gz" => "sha256:28a2046e9250a9e98dc88a407c0455be92e49b454e07d4b55d0e4491029ac9bc",
-  "liblrs-v0.1.2-nif-2.15-x86_64-apple-darwin.so.tar.gz" => "sha256:734fbecc6ea50b9b3373cee0717d5645053470cbc3085c71c2e86da0338a27bf",
-  "liblrs-v0.1.2-nif-2.15-x86_64-unknown-linux-gnu.so.tar.gz" => "sha256:235d790c9a7f976b76f40e01d5235d5ac6cd8d7e4c09c0c9f9169f39a592f309",
-  "liblrs-v0.1.2-nif-2.15-x86_64-unknown-linux-musl.so.tar.gz" => "sha256:a68dc941c7218e9f3fdc756de073a55e82de56522433fff825b3e20e89b2e037",
+  "liblrs-v0.1.3-nif-2.15-aarch64-apple-darwin.so.tar.gz" => "sha256:339b5ccfec4049ed08b1554a8242aa2e38d4316a1887d547206bab510440c8a3",
+  "liblrs-v0.1.3-nif-2.15-aarch64-unknown-linux-gnu.so.tar.gz" => "sha256:fd625fb3d0610b67f94e7905816363394c18afb056e457489547b7793ecf974e",
+  "liblrs-v0.1.3-nif-2.15-arm-unknown-linux-gnueabihf.so.tar.gz" => "sha256:719342a6c64b5bde6698b175eaad2a02111d5838ec2c5f3b8cc83b1412a4ff2a",
+  "liblrs-v0.1.3-nif-2.15-x86_64-apple-darwin.so.tar.gz" => "sha256:980f22104d24cbfbf430a923d1195cf1570099671b407085dac37b2fda12501e",
+  "liblrs-v0.1.3-nif-2.15-x86_64-unknown-linux-gnu.so.tar.gz" => "sha256:b15bb01bb33bc328c8c4f303d675429b49c68751c5d7363a2358695d4d3b57b5",
+  "liblrs-v0.1.3-nif-2.15-x86_64-unknown-linux-musl.so.tar.gz" => "sha256:3a45037e4a42bea7207e92a1123cdf144f2061456c754f07f974e83a8aea7d45",
 }
