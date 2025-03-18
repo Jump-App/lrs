daf5e3f5596103a5f921977cd5a80053b1b5f8a6
Adam Olsen <arolsen@gmail.com>
2025-03-18 14:20:15 -0600

    Update checksums for v0.1.5

--- checksum-Elixir.LRS.exs
+++ checksum-Elixir.LRS.exs
@@ -1,8 +1,2 @@
 %{
-  "liblrs-v0.1.4-nif-2.15-aarch64-apple-darwin.so.tar.gz" => "sha256:f95da42ecf48d1544b2e0824685de988d16efd6d1556b1aa44eaadedce2172f0",
-  "liblrs-v0.1.4-nif-2.15-aarch64-unknown-linux-gnu.so.tar.gz" => "sha256:ee102c591a3e52d86dae6dcd3dc242a149f202db5dea7d7d13d213dc4e10c91e",
-  "liblrs-v0.1.4-nif-2.15-arm-unknown-linux-gnueabihf.so.tar.gz" => "sha256:d78b0dc06717bb8d8d721a9a24472161917ad80ea2ddfc8e61fd43e4b4da0dd2",
-  "liblrs-v0.1.4-nif-2.15-x86_64-apple-darwin.so.tar.gz" => "sha256:8ad03d9947a471837074d9b02e420d355ea82848ca0107779955d0512d4827de",
-  "liblrs-v0.1.4-nif-2.15-x86_64-unknown-linux-gnu.so.tar.gz" => "sha256:0abb28e273a2bd65c66c8cfcaf0ae88dac0292e7d29a0f7a3dd94d1ec7414933",
-  "liblrs-v0.1.4-nif-2.15-x86_64-unknown-linux-musl.so.tar.gz" => "sha256:2a778a60e0c10c164077065200a5964fb5aba1e6c7e4daca837c6b4d7a0205fd",
 }
